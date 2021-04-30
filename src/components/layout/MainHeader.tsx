c2678ea7b1c4bfe0efa437351177b89d5e6b8a00
Francis Lagares <francis.lagares@gmail.com>
2021-04-30 16:28:54 +0200

    feat(layout): create main header

---
+++ src/components/layout/MainHeader.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import classes from '@/styles/main-header.module.css';
+
+const MainHeader = () => (
+  <header className={classes.header}>
+    <div className={classes.logo}>
+      <Link href="/">NextEvents</Link>
+    </div>
+    <nav className={classes.navigation}>
+      <ul>
+        <li>
+          <Link href="/events">Browse all Events</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default MainHeader;
