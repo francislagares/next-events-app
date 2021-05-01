fa2d992b1a8988bfb65fdd965c5ec9cd2a55c109
Francis Lagares <francis.lagares@gmail.com>
2021-05-01 12:30:01 +0200

    feat(events): create ResultsTitle component

---
+++ src/components/events/ResultsTitle.tsx
@@ -0,0 +1,19 @@
+import Button from '@/components/ui/Button';
+import classes from '@/styles/results-title.module.css';
+import { IDate } from '@/types';
+
+const ResultsTitle = ({ date }: IDate) => {
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    month: 'long',
+    year: 'numeric',
+  });
+
+  return (
+    <section className={classes.title}>
+      <h1>Events in {humanReadableDate}</h1>
+      <Button link="/events">Show all events</Button>
+    </section>
+  );
+};
+
+export default ResultsTitle;
