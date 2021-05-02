33cfa63af6f0085e9562bae119503fb6494b9f3b
Francis Lagares <francis.lagares@gmail.com>
2021-05-02 14:16:26 +0200

    refactor(home): add static side generation to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
+import { GetStaticProps } from 'next';
 import EventsList from '@/components/events/EventsList';
-import { getFeaturedEvents } from '@/data/dummy-data';
+import { getFeaturedEvents } from '@/helpers/api-utils';
+import { IEventList } from '@/types';
 
-const Home = () => {
-  const featuredEvents = getFeaturedEvents();
+const Home = ({ events }: IEventList) => (
+  <div>
+    <EventsList events={events} />
+  </div>
+);
 
-  return (
-    <div>
-      <EventsList events={featuredEvents} />
-    </div>
-  );
+export const getStaticProps: GetStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: { events: featuredEvents },
+  };
 };
 
 export default Home;
