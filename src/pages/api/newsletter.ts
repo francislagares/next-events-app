d8bb938879343a9fd5e9d130ad75a5d790fd6497
Francis Lagares <francis.lagares@gmail.com>
2021-05-07 20:43:01 +0200

    feat(api): add mongodb connection insert functionality

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,10 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { connectDatabase, insertDocument } from '@/helpers/db-util';
 
 type Data = {
   message: string;
 };
 
-const newsletterHandler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const newsletterHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) => {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -12,7 +16,23 @@ const newsletterHandler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
       res.status(422).json({ message: 'Invalid email address' });
     }
 
-    console.log(userEmail);
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
+
+    try {
+      await insertDocument(client, 'newsletter', { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!' });
+      return;
+    }
+
     res.status(201).json({ message: 'Signed up!' });
   }
 };
