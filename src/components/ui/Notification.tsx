be46e14dba024ee2edf3e7df3636440723278213
Francis Lagares <francis.lagares@gmail.com>
2021-05-09 20:08:20 +0200

    feat(ui): create notification component

---
+++ src/components/ui/Notification.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useContext } from 'react';
+import NotificationContext from '@/store/NotificationContext';
+import classes from '@/styles/notification.module.css';
+import { INotification } from '@/types';
+
+const Notification = ({ title, message, status }: INotification) => {
+  const notificationCtx = useContext(NotificationContext);
+
+  let statusClasses = '';
+
+  if (status === 'success') {
+    statusClasses = classes.success;
+  }
+
+  if (status === 'error') {
+    statusClasses = classes.error;
+  }
+
+  if (status === 'pending') {
+    statusClasses = classes.pending;
+  }
+
+  const activeClasses = `${classes.notification} ${statusClasses}`;
+
+  return (
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Notification;
